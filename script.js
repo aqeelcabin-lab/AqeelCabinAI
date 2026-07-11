610c52baf810285f21b42617497035146b47884d
aqeelcabin-lab <aqeelcabin@gmail.com>
2026-07-11 10:35:24 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Aqeel Assistant v7 — safer calendar handling and natural FAQ answers
-const API_URL = "https://script.google.com/macros/s/AKfycby0THqdT9zjs8uowwJK_A5BGWEEEGgBTSdz7II2Er18kAtHew_J8ce0jRe8qQ3I_tVAM/exec";
+const API_URL = "https://script.google.com/macros/s/AKfycbzUjzonnZD1J9kwT57PKkfYUDVtunCVhi86czxYpFCaaO6S6EIUNO21umdl9sFqhsjc/exec";
 
 let currentLanguage = "es";
 let currentProperty = "cabin";
